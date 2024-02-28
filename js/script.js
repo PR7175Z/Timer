951c0a72761650c0dec7ff362457ec27ad2262d8
PR7175Z <aadimpraz@gmail.com>
2024-02-28 17:10:56 +0545

    added css and proper ending for time finish using js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,16 @@ document.querySelector('form').addEventListener('submit', function(e){
 	//prevent from page reload on form submission
 	e.preventDefault();
 
-	time = 0;
 	var min = document.getElementsByName('minutes')[0].value;
 	var sec = document.getElementsByName('seconds')[0].value;
+	var msg = document.createElement('p');
+	msg.innerHTML = "Times up";
+
+	if(!min){
+		min = 0;
+	} if(!sec){
+		sec = 0;
+	}
 
 	outputWrap = document.getElementById('output');
 
@@ -13,13 +20,18 @@ document.querySelector('form').addEventListener('submit', function(e){
 	timerWrap.innerHTML = '<span id="counterminute">'+ min + '</span> : <span id="countersecond">' + sec + '</span>';
 
 	outputWrap.appendChild(timerWrap);
-	setInterval(function(){
-		sec = sec - 1;
+	var x = setInterval(function(){
 		if(sec<=0){
 			min = min-1;
 			sec = 60;
 		}
+		sec = sec - 1;
+		if(min == 0 && sec == 0){
+			clearInterval(x);
+			outputWrap.appendChild(msg);
+		}
 		timerWrap.innerHTML = '<span id="counterminute">'+ min + '</span> : <span id="countersecond">' + sec + '</span>';
 		outputWrap.appendChild(timerWrap);
 	}, 1000);
+
 })
