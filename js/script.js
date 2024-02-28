34b213ae9e745f9dea77b918db057d143a206961
PR7175Z <aadimpraz@gmail.com>
2024-02-28 17:38:42 +0545

    adding zero if one digit counter

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ document.querySelector('form').addEventListener('submit', function(e){
 	//prevent from page reload on form submission
 	e.preventDefault();
 
+	outputWrap = document.getElementById('output');
+
 	var min = document.getElementsByName('minutes')[0].value;
 	var sec = document.getElementsByName('seconds')[0].value;
 	var msg = document.createElement('p');
@@ -13,11 +15,14 @@ document.querySelector('form').addEventListener('submit', function(e){
 		sec = 0;
 	}
 
-	outputWrap = document.getElementById('output');
-
 	var timerWrap = document.createElement('div');
 
-	timerWrap.innerHTML = '<span id="counterminute">'+ min + '</span> : <span id="countersecond">' + sec + '</span>';
+	var addval = add_zero_for_one_digit(min, sec);
+
+	var newmin = addval[0];
+	var newsec = addval[1];
+
+	timerWrap.innerHTML = '<span id="counterminute">'+ newmin + '</span> : <span id="countersecond">' + newsec + '</span>';
 
 	outputWrap.appendChild(timerWrap);
 	var x = setInterval(function(){
@@ -30,8 +35,19 @@ document.querySelector('form').addEventListener('submit', function(e){
 			clearInterval(x);
 			outputWrap.appendChild(msg);
 		}
-		timerWrap.innerHTML = '<span id="counterminute">'+ min + '</span> : <span id="countersecond">' + sec + '</span>';
+		addval = add_zero_for_one_digit(min, sec);
+		newmin = addval[0];
+		newsec = addval[1];
+
+		timerWrap.innerHTML = '<span id="counterminute">'+ newmin + '</span> : <span id="countersecond">' + newsec + '</span>';
 		outputWrap.appendChild(timerWrap);
 	}, 1000);
 
-})
+});
+
+function add_zero_for_one_digit(min, sec){
+	var newmin = (min.toString().length === 1)? '0'+min : min;
+	var newsec = (sec.toString().length === 1)? '0'+sec : sec;
+
+	return [newmin, newsec];
+}
