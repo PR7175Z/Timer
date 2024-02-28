f0a5e8d626a83da69b2f1c4e21d7d8edae23ba1b
PR7175Z <aadimpraz@gmail.com>
2024-02-28 16:39:43 +0545

    Changes made to js file for proper working of the timer

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 document.querySelector('form').addEventListener('submit', function(e){
+	//prevent from page reload on form submission
 	e.preventDefault();
+
 	time = 0;
 	var min = document.getElementsByName('minutes')[0].value;
 	var sec = document.getElementsByName('seconds')[0].value;
-	time = min*60 + sec;
-	document.getElementById('output').innerHTML = time
+
+	outputWrap = document.getElementById('output');
+
+	var timerWrap = document.createElement('div');
+
+	timerWrap.innerHTML = '<span id="counterminute">'+ min + '</span> : <span id="countersecond">' + sec + '</span>';
+
+	outputWrap.appendChild(timerWrap);
 	setInterval(function(){
-		document.getElementById('output').innerHTML = time - 1
-		time = time-1;
+		sec = sec - 1;
+		if(sec<=0){
+			min = min-1;
+			sec = 60;
+		}
+		timerWrap.innerHTML = '<span id="counterminute">'+ min + '</span> : <span id="countersecond">' + sec + '</span>';
+		outputWrap.appendChild(timerWrap);
 	}, 1000);
 })
